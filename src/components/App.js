28489b6ee4e77fc120b70409017ec1d9ee3ef8c5
Kepitss <keper524@gmail.com>
2021-11-18 10:26:53 +0200

    Set up App with bootstrap and wrap a title in it
    - Now this aplication uses react-bootstrap and bootstrap wich gona take
      care most of the styles.
    - "To Do List"  title is now added too.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+	return (
+		<Container fluid="xs">
+			<h1 className="text-center">To Do List</h1>
+		</Container>
+	);
+};
+
+export default App;
